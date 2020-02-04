36c4f72cd5119eabe8d73fc7a2030f1020089c0f
Oscar Varela <nefthalyvarela@gmail.com>
2020-02-03 21:44:38 -0600

    Initial Commit

---
+++ Actividad 2.js
@@ -0,0 +1,17 @@
+var mes ='10'
+
+if((mes>=9) && (mes<=11)){
+  alert("es Otoño")
+}
+if(mes==12 || mes==1 || mes==2){
+  alert("es Invierno")
+}
+if(mes==3 || mes==4 || mes==5){
+  alert("es Primavera")
+}
+if(mes==6 || mes==7 || mes==8){
+  alert("es Verano")
+}
+{
+  alert("Oscar Neftali Varela Mena 20002716")
+}
